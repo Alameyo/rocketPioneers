300132ceed37b83023de5383c6da71037d20e056
Alameyo <Alameyo@users.noreply.github.com>
2016-05-08 23:08:32 +0200

    Added Flyer Class

---
+++ brejnRush-core/src/com/mygdx/brejnrush/Flyer.java
@@ -0,0 +1,28 @@
+package com.mygdx.gagarin;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Flyer extends Rectangle {
+	private Texture textura1;
+	private float acceleration;
+	public Flyer(Texture textura1, float acceleration, Rocket rakieta){
+		this.textura1 = textura1;
+		this.acceleration = acceleration;
+		this.height = this.textura1.getHeight()-10;
+		this.width = this.textura1.getWidth()-200;
+		this.x = MathUtils.random(-1000f+rakieta.x, +1000f+rakieta.x);
+		this.y = rakieta.y +2000;
+	}
+	public Texture getTextura1() {
+		return textura1;
+	}
+	public float getAcceleration() {
+		return acceleration;
+	}
+	public void drawFlyer(SpriteBatch batch) {
+		batch.draw(this.textura1, this.x, this.y, 200, 150);
+	}
+}
