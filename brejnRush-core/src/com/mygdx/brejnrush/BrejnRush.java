55d4634904e05b684259af2b79c58df48c1ad8b1
Alameyo <Alameyo@users.noreply.github.com>
2016-05-10 23:42:10 +0200

    Score, Flyers live
    Now Flyers disappear after hit. Added time and score.

--- brejnRush-core/src/com/mygdx/brejnrush/BrejnRush.java
+++ brejnRush-core/src/com/mygdx/brejnrush/BrejnRush.java
@@ -1,8 +1,9 @@
-package com.mygdx.brejnrush;
+package com.mygdx.gagarin;
 
 import java.util.LinkedList;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
@@ -14,7 +15,7 @@ import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MainClass extends ApplicationAdapter {
+public class MainClass extends Game {
 	private OrthographicCamera cam;
 	private SpriteBatch batch;
 	// private SpriteBatch backgroundBatch;
@@ -24,22 +25,25 @@ public class MainClass extends ApplicationAdapter {
 	private BitmapFont font;
 	private Rocket wostok, soyuz;
 	private float timeHelper;
-	
+
 	private Music music;
 
-
 	private Texture background;
-	
+
 	private float gravity;
-	
+
 	private Texture ufok;
 	private LinkedList<Flyer> szkodniki;
-	int collets;
-	int counter;
+	private int collets;
+	private int counter;
+	private double startTime;
+	private double currentTime;
+	private double score;
+	private boolean gameOver;
 
 	@Override
 	public void create() {
-		
+
 		cam = new OrthographicCamera(1400, 800);
 		batch = new SpriteBatch();
 
@@ -49,7 +53,7 @@ public class MainClass extends ApplicationAdapter {
 		ufok = new Texture("ufok.png");
 		rocketVostok = new Texture("rokieta_vostok.png");
 		plomienGraphic = new Texture("plomien.png");
-		
+
 		font = new BitmapFont();
 		font.setColor(Color.WHITE);
 		font.getData().setScale(2);
@@ -58,13 +62,13 @@ public class MainClass extends ApplicationAdapter {
 		wostok = new Rocket(rocketVostok, 2.7f);
 		wostok.x = 100;
 		wostok.y = 10;
-		
+
 		wostok.height = 200;
 		wostok.width = 80;
-		
+
 		counter = 0;
 		collets = 0;
-		
+
 		soyuz = new Rocket(rocketVostok, 10f);
 		soyuz.x = 300;
 		soyuz.y = 10;
@@ -76,7 +80,9 @@ public class MainClass extends ApplicationAdapter {
 		music.setVolume(0.08f);
 
 		szkodniki = new LinkedList<Flyer>();
-		
+		startTime = System.currentTimeMillis();
+		currentTime =0;
+		gameOver=false;
 	}
 
 	@Override
@@ -88,35 +94,44 @@ public class MainClass extends ApplicationAdapter {
 
 		Gdx.gl.glClearColor(10, 10, 10, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		batch.draw(background, wostok.x - 1500, -50, 3000, 300000);
-		
+
 		wostok.drawRocket(batch);
-		
+
 		batch.draw(soyuz.getTextura1(), soyuz.x, soyuz.y);
 		font.draw(batch, "Poyehali na: " + wostok.y, cam.position.x - 650, cam.position.y - 300);
-		font.draw(batch, "Damage: " + collets,cam.position.x + 520, cam.position.y - 300);
+		font.draw(batch, "Damage: " + collets, cam.position.x + 520, cam.position.y - 300);
+		font.draw(batch, "Current X: " + wostok.x, cam.position.x + 520, cam.position.y - 200);
+		font.draw(batch, "Time: " + currentTime, cam.position.x + 520, cam.position.y - 100);
+		if(wostok.y >=300000 || gameOver ==true){
+			font.draw(batch, "Score: " + score , cam.position.x + 520, cam.position.y);
+		
+		}
+		
+		for (int i = 0; i < szkodniki.size(); i++) {
 
-		for(int i = 0; i<szkodniki.size(); i++){
-		szkodniki.get(i).drawFlyer(batch);
+			if (szkodniki.get(i).isLive() == true) {
+				szkodniki.get(i).drawFlyer(batch);
+			}
 		}
 		batch.end();
 	}
 
 	private void update() {
 		cam.update();
-		
+
 		batch.setProjectionMatrix(cam.combined);
-		cam.position.set(wostok.x + wostok.width / 2, wostok.y + wostok.height + 300 , 0);
+		cam.position.set(wostok.x + wostok.width / 2, wostok.y + wostok.height + 300, 0);
 
 		if (Gdx.input.isKeyPressed(Keys.SHIFT_LEFT)) {
 			cam.zoom = 10.02f;
 		} else {
 			cam.zoom = 2f;
 		}
-		
+
 		wostok.move();
-		
+
 		if (wostok.overlaps(soyuz)) {
 			wostok.x -= 250 * Gdx.graphics.getDeltaTime();
 		}
@@ -131,19 +146,24 @@ public class MainClass extends ApplicationAdapter {
 				music.play();
 			}
 		}
-		if((wostok.y)/(400)>counter){
-			
+		if ((wostok.y) / (400) > counter) {
+
 			szkodniki.add(counter, new Flyer(ufok, gravity, wostok));
 			counter++;
 
 		}
-		for(int i =0; i<szkodniki.size(); i++){
-		
-			if(wostok.overlaps(szkodniki.get(i))){
+		for (int i = 0; i < szkodniki.size(); i++) {
+
+			if (wostok.overlaps(szkodniki.get(i))&&szkodniki.get(i).isLive()==true) {
+				szkodniki.get(i).setLive(false);
 				collets++;
 			}
 		}
-		
+		currentTime =(System.currentTimeMillis() - startTime)/1000;
+		if(wostok.y >=300000 && gameOver==false){
+			score= 300000/(currentTime + collets);
+			gameOver = true;
+		}
 	}
 
 	@Override
