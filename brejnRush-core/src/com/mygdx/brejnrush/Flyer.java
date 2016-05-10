644f460bf7617122189e03481fedee1151cf9a4c
Alameyo <Alameyo@users.noreply.github.com>
2016-05-10 23:43:08 +0200

    Live
    Added live parametr for Flyers

--- brejnRush-core/src/com/mygdx/brejnrush/Flyer.java
+++ brejnRush-core/src/com/mygdx/brejnrush/Flyer.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.math.Rectangle;
 public class Flyer extends Rectangle {
 	private Texture textura1;
 	private float acceleration;
+	private boolean live;
 	public Flyer(Texture textura1, float acceleration, Rocket rakieta){
 		this.textura1 = textura1;
 		this.acceleration = acceleration;
@@ -15,6 +16,13 @@ public class Flyer extends Rectangle {
 		this.width = this.textura1.getWidth()-200;
 		this.x = MathUtils.random(-1000f+rakieta.x, +1000f+rakieta.x);
 		this.y = rakieta.y +2000;
+		this.live = true;
+	}
+	public void setLive(boolean live) {
+		this.live = live;
+	}
+	public boolean isLive() {
+		return live;
 	}
 	public Texture getTextura1() {
 		return textura1;
